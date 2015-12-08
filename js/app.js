969235020e625da00062054300c03c985a948531
Eduardo Gutierrez <eldelentes@MacBook-Pro-de-Eduardo.local>
2015-12-08 15:03:20 -0600

    Adds projects

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
+$('.categories-nav a').click(function(){
+  var cat = $(this).data('cat');
+  $('.category').hide();
+  $('.projects').find('[data-cat="'+ cat + '"]').show().addClass('animated fadeInUp');
+});
+
+$('#all').click(function(){
+  $('.category').show().addClass('animated fadeInUp');
+});
+
+
 $(function(){
   var header = $('.main-header');
   var x = header.offset().top;
   $(window).scroll(function(e){
     var y = $(window).scrollTop();
-      console.log(y);
       e.preventDefault();
       if (y >= x ) {
         header.addClass('fixed');
@@ -12,3 +22,4 @@ $(function(){
       }
   })
 });
+
